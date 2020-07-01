5192d25339602e9154eda5e5b12b5d09ddc11752
infrecursion <alokendra+1@gmail.com>
2020-07-02 01:17:31 +0530

    Added a helper script for virtual environment
    Added a helper script to install virtual environment.

---
+++ Virtualenv.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Script to setup a virtual environment for viewing jupyter notebook
+# virtual environment must be installed, python interpreter should
+# be > 2.7.15
+
+set -e
+
+VIRTUALENVNAME=pyenv
+NOTEBOOKFILE="Cell_Volume_Aneuploidy_Osmotic_Stress.md"
+
+virtualenv ./$VIRTUALENVNAME
+echo "Virtual environment $VIRTUALENVNAME installed, switching"
+source ./$VIRTUALENVNAME/bin/activate
+echo "New python executable:"
+which python
+read -p "Install necessary packages?(y/n): " Res
+if [ "$Res" != "y" ] ; then
+    echo "Exiting...."
+    exit 1
+fi
+echo "Installing packages, this may take a while...."
+pip install -r requirements.txt > Piplog
+Status=$?
+if [ $Status -eq 0 ] ; then
+    echo "Installation successful"
+    echo "Syncing jupyter notebook..."
+    jupytext --sync "$NOTEBOOKFILE"
+    NOTEBOOKIPYNB="${NOTEBOOKFILE%.md}".ipynb
+    read -p "Open notebook file $NOTEBOOKIPYNB ?(y/n): " Op
+    if [ "$Op" = "y" ] ; then
+	jupyter notebook "$NOTEBOOKIPYNB"
+    else
+	Command="jupyter notebook $NOTEBOOKIPYNB"
+	echo "Open the notebook with command: \"$Command\""
+	echo "End of script.."
+    fi
+else
+    echo "Installation unsuccessful, see the log for details"
+fi
+
+
